9f91b2ea6bc7c1682278daac116814b3f10c5859
MIR0V0Y <ya.mirovoy@yandex.ru>
2025-07-06 19:04:00 +0300

    Вариант *другое

--- main.js
+++ main.js
@@ -1,31 +1,37 @@
 tg.MainButton.setText("Вдарил!");
 tg.MainButton.onClick(MBC);
 tg.MainButton.show();
-tg.MainButton.disable();
+tg.MainButton.hasShineEffect = true;
 
 function MBC() {
     // TODO: Выполнить удар
 }
-// Функция для проверки заполнения полей
-function checkFields() {
-    const volume = document.getElementById('volume').value;
-    const percent = document.getElementById('percent').value;
-    const brand = document.getElementById('brand').value;
-    const price = document.getElementById('price').value;
+
+// Обработчики для показа/скрытия текстовых полей при выборе "Другое"
+const selectFields = [
+    { selectId: 'brand', otherId: 'brand-other' },
+    { selectId: 'percent', otherId: 'percent-other' },
+    { selectId: 'volume', otherId: 'volume-other' },
+    { selectId: 'price', otherId: 'price-other' }
+];
+
+function setupSelectHandler(selectId, otherId) {
+    const select = document.getElementById(selectId);
+    const other = document.getElementById(otherId);
     
-    if (volume && percent) {
-        tg.MainButton.enable();
-        if (brand && price) {
-            tg.MainButton.hasShineEffect = true;
+    select.addEventListener('change', function() {
+        if (this.value === 'Другое') {
+            other.style.display = 'block';
+            other.focus();
         } else {
-            tg.MainButton.hasShineEffect = false;
+            other.style.display = 'none';
+            other.value = '';
         }
-    } else {
-        tg.MainButton.disable();
-    }
+    });
 }
 
-// Добавляем слушатели событий для полей volume и percent
-document.getElementById('volume').addEventListener('input', checkFields);
-document.getElementById('percent').addEventListener('input', checkFields);
+// Устанавливаем обработчики для всех полей
+selectFields.forEach(field => {
+    setupSelectHandler(field.selectId, field.otherId);
+});
 
