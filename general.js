df3eedea10250c710c3dbbc00e94bd0e837bdd17
MIR0V0Y <ya.mirovoy@yandex.ru>
2025-07-06 14:09:02 +0300

    Base

---
+++ general.js
@@ -0,0 +1,6 @@
+var tg = window.Telegram.WebApp;
+
+tg.SettingsButton.onClick(() => {
+    window.location.href = '/settings/settings.html';
+});
+tg.SettingsButton.show();
---
+++ main.js
@@ -0,0 +1,31 @@
+tg.MainButton.setText("Вдарил!");
+tg.BackButton.onClick(MBC);
+tg.MainButton.show();
+MainButton.disable();
+
+function MBC() {
+    // TODO: Выполнить удар
+}
+// Функция для проверки заполнения полей
+function checkFields() {
+    const volume = document.getElementById('volume').value;
+    const percent = document.getElementById('percent').value;
+    const brand = document.getElementById('brand').value;
+    const price = document.getElementById('price').value;
+    
+    if (volume && percent) {
+        MainButton.enable();
+        if (brand && price) {
+            MainButton.hasShineEffect = true;
+        } else {
+            MainButton.hasShineEffect = false;
+        }
+    } else {
+        MainButton.disable();
+    }
+}
+
+// Добавляем слушатели событий для полей volume и percent
+document.getElementById('volume').addEventListener('input', checkFields);
+document.getElementById('percent').addEventListener('input', checkFields);
+
