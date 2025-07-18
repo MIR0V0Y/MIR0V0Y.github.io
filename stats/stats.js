6fa4094090954e32a070ac0c08345bb120113cb0
MIR0V0Y <ya.mirovoy@yandex.ru>
2025-07-18 21:12:40 +0300

    test loading anim

--- stats/stats.js
+++ stats/stats.js
@@ -9,15 +9,15 @@ function SBC () {
 
 function updateStats (response) {
     stats = response.message[0];
-    document.getElementById('drinks-24h').textContent = stats[1];
-    document.getElementById('liters-24h').textContent = stats[2];
-    document.getElementById('alcohol-24h').textContent = stats[3];
-    document.getElementById('drinks-7d').textContent = stats[4];
-    document.getElementById('liters-7d').textContent = stats[5];
-    document.getElementById('alcohol-7d').textContent = stats[6];
-    document.getElementById('drinks-30d').textContent = stats[7];
-    document.getElementById('liters-30d').textContent = stats[8];
-    document.getElementById('alcohol-30d').textContent = stats[9];
+    document.getElementById('drinks-24h').innerHTML = stats[1];
+    document.getElementById('liters-24h').innerHTML = stats[2];
+    document.getElementById('alcohol-24h').innerHTML = stats[3];
+    document.getElementById('drinks-7d').innerHTML = stats[4];
+    document.getElementById('liters-7d').innerHTML = stats[5];
+    document.getElementById('alcohol-7d').innerHTML = stats[6];
+    document.getElementById('drinks-30d').innerHTML = stats[7];
+    document.getElementById('liters-30d').innerHTML = stats[8];
+    document.getElementById('alcohol-30d').innerHTML = stats[9];
 }
 
 SBC();
